2a359034edc70db641f25f09304228d5cfcfdfef
Jack King <email@qiaohong.org>
2018-05-20 00:59:30 +0800

    add default config

---
+++ config/default.py
@@ -0,0 +1,10 @@
+"""
+default config in this file
+"""
+
+default_config = {
+    "LISTENING_ADDRESS": "127.0.0.1",
+    "LISTENING_PORT": "8888",
+    "ENABLE_MEMCACHED": False,
+    "MEMCACHED_URI": "localhost:11211"
+}
