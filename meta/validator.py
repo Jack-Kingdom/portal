6809cc3f1348a15feb0d25fbf752d9e6f2135537
Jack King <email@qiaohong.org>
2018-05-20 00:42:42 +0800

    add basic validator

--- meta/validator.py
+++ meta/validator.py
@@ -2,3 +2,19 @@
 this file supplied some function that \
 used to check url is illegal or not.
 """
+
+import re
+from utils import decr
+
+
+@decr.SingletonDecorator
+class Validator(object):
+    def __init__(self):
+        self.unresolved_char_re = re.compile(r'[0-9a-zA-Z\-_.~]+')
+
+    def contains_unresolved_char_only(self, url):
+        return bool(self.unresolved_char_re.fullmatch(url))
+
+    def is_url_legal(self, url):
+        pass
+        # todo add url legal check here
