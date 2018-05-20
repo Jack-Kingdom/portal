cc2f29303652ab317d2a41d64267a91bd75641a8
Jack King <email@qiaohong.org>
2018-05-20 19:27:19 +0800

    add env overwrite config support

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,15 @@
+"""
+load default config and overwrite with
+environment variable.
+"""
+
+import os
 from .default import default_config
 
 CONFIG = default_config
+
+for key in default_config.keys():
+    if key in os.environ:
+        env_option = os.environ.get(key)
+        origin_type = type(default_config.get(key))
+        CONFIG[key] = origin_type(env_option)
