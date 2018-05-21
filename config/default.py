426954ab687e1687afb701841fdd11fd2a29f13f
Jack King <email@qiaohong.org>
2018-05-21 18:20:44 +0800

    add REDIRECT_STATUS_CODE config

--- config/default.py
+++ config/default.py
@@ -1,11 +1,19 @@
 """
 default config in this file
+ODBC URI Scheme:
+- https://tools.ietf.org/html/draft-patrick-lambert-odbc-uri-scheme-00
 """
 
 default_config = {
     "DEBUG": False,
+
     "LISTENING_ADDRESS": "127.0.0.1",
     "LISTENING_PORT": "8888",
+
+    "DATABASE_URI": "",
+
     "ENABLE_MEMCACHED": False,
-    "MEMCACHED_URI": "localhost:11211"
+    "MEMCACHED_URI": "localhost:11211",
+
+    "REDIRECT_STATUS_CODE": 302
 }
