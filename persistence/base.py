a5cc6dfdd8ce49f13cd0fd9b7690c1471f363e32
Jack King <email@qiaohong.org>
2018-05-21 10:29:53 +0800

    add BaseModel class

---
+++ persistence/base.py
@@ -0,0 +1,16 @@
+"""
+base model for shortURL and alias
+wrapped some shared function here
+"""
+
+import pyodbc
+from config import CONFIG
+
+
+class BaseModel(object):
+    def __init__(self, template):
+        self.conn = pyodbc.connect(CONFIG['DATABASE_URI'])
+        self.cursor = self.conn.cursor()
+
+        self.cursor.execute(template['init'])
+        self.cursor.commit()
