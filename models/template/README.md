c744759fc0d2b651de59f1cf230323ec09e562ec
Jack King <email@qiaohong.org>
2018-05-30 17:36:37 +0800

    rm unused template

--- models/template/README.md
+++
@@ -1,6 +0,0 @@
-# template
-
-yeah, I'm stuck...
-
-It's so stupid to assume that different database has same sql schema.
-
--- models/template/__init__.py
+++
@@ -1,6 +0,0 @@
-from config import CONFIG
-
-if CONFIG['ODBC_DSN']:
-    from .mysql import *
-else:
-    from .sqlite import *
